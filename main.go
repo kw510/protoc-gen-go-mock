ec73b023fd2c245a85c35b9ce1e2a41e3095629b
Kieran Warwick <kwarwick97@gmail.com>
2023-07-27 22:11:41 +0100

    version 0.1.0

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"google.golang.org/protobuf/types/pluginpb"
 )
 
-const version = "0.0.0"
+const version = "0.1.0"
 
 func main() {
 	showVersion := flag.Bool("version", false, "print the version and exit")
